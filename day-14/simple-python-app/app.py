91b3b4e62b99bf7cc9ecd3ea4ea5bbb69829170f
Sivarajvarma <154302164+Sivarajvarma@users.noreply.github.com>
2024-03-22 00:06:48 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, world!'
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
-
+    # Change the host to '0.0.0.0' to listen on all network interfaces
+    app.run(host='0.0.0.0', port=5000)
